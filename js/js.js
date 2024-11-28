c78b0987c79a03a3323cc3d79991a4600fc7cd9a
annalariss <119774083+annalariss@users.noreply.github.com>
2024-11-28 12:04:59 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,5 @@
+// Exemplo de script para interatividade no futuro
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Página carregada com sucesso!');
+    // Aqui você pode adicionar interatividade, como animações ou efeitos
+});
