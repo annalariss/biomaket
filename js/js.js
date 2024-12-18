6f34618b9a25b658cbed1540ed6251b35bef573d
luana-18 <luana.fabianne@escolar.ifrn.edu.br>
2024-12-18 15:15:50 -0300

    explorar, nanotubo, nanorrobo e artigos

--- js/js.js
+++ js/js.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Página carregada com sucesso!');
     // Aqui você pode adicionar interatividade, como animações ou efeitos
 });
+
+// Função para exibir a mensagem de sucesso após o envio do formulário
+function showSuccessMessage(event) {
+    event.preventDefault(); // Previne o comportamento padrão de envio do formulário
+    document.getElementById('quiz-form').classList.add('d-none'); // Oculta o formulário
+    document.getElementById('success-message').classList.remove('d-none'); // Exibe a mensagem de sucesso
+}
+
+// Adiciona o ouvinte de evento no formulário para quando ele for enviado
+document.getElementById('quiz-form').addEventListener('submit', showSuccessMessage);
+
